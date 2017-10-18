1581a7d649d2542473e4e8291e3937d843f81fa6
Sepehr Aliakbari <Sepehr.aliakbari71@gmail.com>
2017-10-18 11:05:56 +0330

    Create menu.js

---
+++ app/styles/menu.js
@@ -0,0 +1,14 @@
+import {StyleSheet} from 'react-vr';
+export default StyleSheet.create({
+    container:{
+        display:'flex',
+        flexDirection:'row'
+    },
+    item: {
+        layoutOrigin: [0.5, 0.5],
+        borderRadius: 0.01,
+    },
+    title: {
+        color: 'black',
+    }
+});
